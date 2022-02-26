3277f3f8857fa043fa9cb32c3eabf0a3cf13f668
samsperd <iloejeobinna@gmail.com>
2022-02-26 10:58:50 +0100

    first commit

---
+++ src/components/Product.vue
@@ -0,0 +1,31 @@
+<template>
+      <v-row>
+        <v-col
+          v-for="n in 3"
+          :key="n"
+          cols="6"
+          md="4"
+          sm="4"
+          lg="4"
+        >
+          <v-item v-slot="{ active, toggle }">
+            <v-card
+              :color="active ? 'primary' : ''"
+              class="d-flex align-center"
+              dark
+              height="200"
+              @click="toggle"
+            >
+              <v-scroll-y-transition>
+                <div
+                  v-if="active"
+                  class="text-h2 flex-grow-1 text-center"
+                >
+                  Active
+                </div>
+              </v-scroll-y-transition>
+            </v-card>
+          </v-item>
+        </v-col>
+      </v-row>
+</template>
